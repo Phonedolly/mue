6bcd55aaed2f34affb99841d721d1b8479904b85
Phonedolly <Phonedolly@users.noreply.github.com>
2023-05-16 15:36:14 +0900

    apply global style using styled-components

--- mue-frontend/src/globalStyle.ts
+++ mue-frontend/src/globalStyle.ts
@@ -1,7 +1,23 @@
+// global.ts
 import { createGlobalStyle } from "styled-components";
+import OutfitBold from "./fonts/Outfit-Bold.woff2";
+import OutfitRegular from "./fonts/Outfit-Regular.woff2";
 
 const GlobalStyle = createGlobalStyle`
-  background-color:transparent;
+  @font-face {
+        font-family: 'OutfitBold';
+        src: local('OutfitBold'), local('OutfitBold');
+        font-style: normal;
+        src: url(${OutfitBold}) format('truetype');
+  }
+  @font-face {
+        font-family: 'OutfitRegular';
+        src: local('OutfitRegular'), local('OutfitRegular');
+        font-style: normal;
+        src: url(${OutfitRegular}) format('truetype');
+  }
+
+  background-color: transparent;
 `;
 
 export default GlobalStyle;
